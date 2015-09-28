0012c35b53372f3667be23f01fddc310002938f1
Derek Choi <claztec@gmail.com>
2015-09-28 17:04:37 +0900

    add angularjs tutorial

--- bonito.py
+++ bonito.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
+# @app.route('/phones/phones.json')
+# def phones():
+#     return render_template('phones.json')
 
 if __name__ == '__main__':
     app.run()
---
+++ static/js/filters.js
@@ -0,0 +1,8 @@
+var phonecatFilters = angular.module('phonecatFilters',[]);
+
+phonecatFilters.filter('checkmark', function() {
+    return function(input) {
+        return input ? '\u2713' : '\u2718';
+    };
+});
+
